fee316b1aac8e03fc86274d0860b275ea6acc1ab
dcanchari <dtcancharir@gmail.com>
2025-07-27 01:17:28 -0500

    RequestMapping - Sending data to the view

--- src/main/java/com/project/springbootcourse/controller/EjemploRestController.java
+++ src/main/java/com/project/springbootcourse/controller/EjemploRestController.java
@@ -1,19 +1,23 @@
 package com.project.springbootcourse.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/api")
 public class EjemploRestController {
 
-    @GetMapping("/detalles_info2")
+    @RequestMapping(path = "/detalles_info2", method = RequestMethod.GET)
     public Map<String, Object> detalles_info2() {
         Map<String, Object> respuesta = new HashMap<>();
         respuesta.put("Titulo", "Servidor en línea");
         respuesta.put("Servidor", "Dulcecr");
+        respuesta.put("IP", "191.168.1.1");
         return respuesta;
     }
 }
